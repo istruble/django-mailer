50bd5fd56e83d975f5c0aadb03be17fc49261e60
Ian Struble <istruble@gmail.com>
2009-05-23 14:15:13 -0700

    Reusing SMTP connections for sending e-mails.
    http://github.com/jtauber/django-mailer/issues/#issue/2
    
    Note: Connections are only reused on success.

--- mailer/engine.py
+++ mailer/engine.py
@@ -7,7 +7,7 @@ from socket import error as socket_error
 from mailer.models import Message, DontSendEntry, MessageLog
 
 from django.conf import settings
-from django.core.mail import send_mail as core_send_mail
+from django.core.mail import EmailMessage
 
 # when queue is empty, how long to wait (in seconds) before checking again
 EMPTY_QUEUE_SLEEP = getattr(settings, "MAILER_EMPTY_QUEUE_SLEEP", 30)
@@ -15,6 +15,10 @@ EMPTY_QUEUE_SLEEP = getattr(settings, "MAILER_EMPTY_QUEUE_SLEEP", 30)
 # lock timeout value. how long to wait for the lock to become available.
 # default behavior is to never wait for the lock to be available.
 LOCK_WAIT_TIMEOUT = getattr(settings, "MAILER_LOCK_WAIT_TIMEOUT", -1)
+
+# a DRY_RUN will not create SMTP connections or attempt to send msgs.
+DRY_RUN = getattr(settings, "MAILER_DRY_RUN", False)
+SKIP_SEND = getattr(settings, "MAILER_DRY_RUN_SKIP_SEND", DRY_RUN)
 
 
 def prioritize():
@@ -58,6 +62,7 @@ def send_all():
     dont_send = 0
     deferred = 0
     sent = 0
+    connection = None
     
     try:
         for message in prioritize():
@@ -69,12 +74,25 @@ def send_all():
             else:
                 try:
                     logging.info("sending message '%s' to %s" % (message.subject.encode("utf-8"), message.to_address.encode("utf-8")))
-                    core_send_mail(message.subject, message.message_body, message.from_address, [message.to_address])
+                    msg = EmailMessage(
+                        subject=message.subject,
+                        body=message.message_body,
+                        from_email=message.from_address,
+                        to=[message.to_address],
+                        connection=connection)
+                    if not DRY_RUN:
+                        if connection is None:
+                            # save the connection for possible reuse
+                            connection = msg.get_connection()
+                            logging.debug("got new connection")
+                        if not SKIP_SEND:
+                            msg.send()
                     MessageLog.objects.log(message, 1) # @@@ avoid using literal result code
                     message.delete()
                     sent += 1
                 except (socket_error, smtplib.SMTPSenderRefused, smtplib.SMTPRecipientsRefused, smtplib.SMTPAuthenticationError), err:
                     message.defer()
+                    connection = None # don't try to cache on error
                     logging.info("message deferred due to failure: %s" % err)
                     MessageLog.objects.log(message, 3, log_message=str(err)) # @@@ avoid using literal result code
                     deferred += 1
